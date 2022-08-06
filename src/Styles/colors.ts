c8d39d5045dd6d5df7acffdba954f8ff22937bee
Geunta Buwono <25718928+GeuntaBuwono@users.noreply.github.com>
2022-08-07 02:19:43 +0700

    chore: list all color for project

---
+++ src/Styles/colors.ts
@@ -0,0 +1,11 @@
+const Colors = {
+  darkGrey: '#2C2C2B',
+  silver: '#B1B1B1',
+  pageGray: '#F4F4F4',
+  redPink: '#FA275A',
+  topaz: '#5BC4BA',
+  white: '#FFF',
+  yellow: '#FFDD00',
+}
+
+export default Colors
