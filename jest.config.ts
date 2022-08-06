9a2997b5699b86492b4f53643d4e13a51be8bf1d
Geunta Buwono <25718928+GeuntaBuwono@users.noreply.github.com>
2022-08-07 00:38:08 +0700

    chore: add react-navigation

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
+  verbose: true,
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFiles: ['<rootDir>/jest/setup.js'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|rollbar-react-native|@fortawesome|@react-native|@react-navigation)',
+  ],
+}
+
+export default config
