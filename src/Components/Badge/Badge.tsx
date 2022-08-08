e418636cb691780bee01dfeb2d12a53cfd624444
Geunta Buwono <25718928+GeuntaBuwono@users.noreply.github.com>
2022-08-09 05:22:56 +0700

    feat: add variant on Badge

--- src/Components/Badge/Badge.tsx
+++ src/Components/Badge/Badge.tsx
@@ -4,25 +4,33 @@ import { View } from 'react-native'
 import styled from 'styled-components'
 import Colors from 'Styles/colors'
 
-import { BadgeColorType, BadgeInterface, BadgeStyleInterface } from './Badge.type'
+import { BadgeColorType, BadgeInterface, BadgeSizeType, BadgeStyleInterface } from './Badge.type'
 
 const badgeColorMapper: Record<BadgeColorType, string> = {
   grey: Colors.silver,
   topaz: Colors.topaz,
   red: Colors.redPink,
 }
+const badgeSizeMapper: Record<BadgeSizeType, string> = {
+  small: '2px 8px',
+  medium: '6px 12px',
+  large: '6px 16px',
+}
 
 const BadgeStyled = styled(View)<BadgeStyleInterface>`
   background-color: ${(props) => badgeColorMapper[props.color]};
   border-radius: 6px;
-  padding: 5px 10px;
+  padding: ${(props) => badgeSizeMapper[props.size ?? 'medium']};
 `
 
 const Badge = (props: BadgeInterface) => {
-  const { label, color } = props
+  const { label, color, size } = props
   return (
-    <BadgeStyled color={color}>
-      <Label restStyle={{ color: Colors.white, textAlign: 'center', fontWeight: 'bold' }}>
+    <BadgeStyled color={color} size={size}>
+      <Label
+        restStyle={{ color: Colors.white, textAlign: 'center', fontWeight: 'bold' }}
+        sizeVariant="small"
+      >
         {label}
       </Label>
     </BadgeStyled>
--- src/Components/Badge/Badge.type.ts
+++ src/Components/Badge/Badge.type.ts
@@ -1,7 +1,9 @@
 export type BadgeColorType = 'topaz' | 'grey' | 'red'
+export type BadgeSizeType = 'small' | 'medium' | 'large'
 
 export interface BadgeStyleInterface {
   color: BadgeColorType
+  size?: BadgeSizeType
 }
 export interface BadgeInterface extends BadgeStyleInterface {
   label: string
