a49dff14cb67ceb2091c39a84d02a60ac68387ca
Geunta Buwono <25718928+GeuntaBuwono@users.noreply.github.com>
2022-08-09 09:03:56 +0700

    feat: add MMMM yyyy and d MMMM yyyy date format on dateFormatter

--- src/Utils/dateFormatter.ts
+++ src/Utils/dateFormatter.ts
@@ -1,6 +1,14 @@
 import * as dateFns from 'date-fns'
 
-type DateFormatType = 'HH:MM' | 'iiii, d MMM yyyy' | 'd MMM' | 'iiii' | 'iii' | 'd' | 'MMMM yyyy'
+type DateFormatType =
+  | 'HH:MM'
+  | 'iiii, d MMM yyyy'
+  | 'd MMM'
+  | 'iiii'
+  | 'iii'
+  | 'd'
+  | 'MMMM yyyy'
+  | 'd MMMM yyyy'
 
 export const dateFormatter = ({ date, format }: { date: Date; format: DateFormatType }) => {
   return dateFns.format(date, format)
